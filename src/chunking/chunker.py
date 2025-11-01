ee9cc1cc887ce0a68a46437b3714ca34defc7cd9
levont <5532305+levontumanyan@users.noreply.github.com>
2025-10-31 20:33:28 -0400

    some additional debug logging and if last chunk too small in a file, merge with second last...

--- src/chunking/chunker.py
+++ src/chunking/chunker.py
@@ -16,8 +16,16 @@ def chunk_text(text: str, chunk_size: int = 1000, overlap: int = 200) -> list[st
 	while start < len(text):
 		end = start + chunk_size
 		chunks.append(text[start:end])
+		logger.debug(f"Chunk [{start}:{end}]")
 		start = end - overlap
 
+	# merge last chunk if it's too small
+	if len(chunks) > 1 and len(chunks[-1]) < overlap:
+		logger.debug(f"Last chunk is too small({len(chunks[-1])}), merging...")
+		chunks[-2] += chunks[-1]
+		chunks.pop()
+
+	logger.debug(f"{len(chunks)} chunks generated.")
 	return chunks
 
 def generate_chunk_metadata(chunk: str, knowledge_file: Path) -> dict:
@@ -39,8 +47,12 @@ def chunk_file(knowledge_file: Path) -> list[Path]:
 
 	file_chunks_metadata = []
 
+	num_of_chunks = 0
+
 	for chunk in file_chunks:
+		num_of_chunks += 1
 		file_chunks_metadata.append(generate_chunk_metadata(chunk, knowledge_file))
+		logger.debug(f"Chunk {num_of_chunks}:{knowledge_file.name} loaded to metadata dict.")
 
 	return file_chunks_metadata
 
